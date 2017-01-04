ef8b3885332bd386e26d756902e390097edca3a5
Mengxu <taomengxu@126.com>
2017-01-04 16:00:02 +0800

    Init the project

---
+++ src/SimpleCompiler.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Mengxu on 2017/1/4.
+ */
+public class SimpleCompiler
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, this is a simple compiler!");
+    }
+}
