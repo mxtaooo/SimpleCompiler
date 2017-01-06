751a92fa341565542d13d644e39d844e3cb3d862
Mengxu <taomengxu@126.com>
2017-01-06 20:54:47 +0800

    Add and complete the Token class which contains token kinds and the toString method Add and complete the Lexer class Add the lexer introduction file which contains the grammar and an example program

---
+++ blogs/lexer.md
@@ -0,0 +1,87 @@
+## Lexer
+
+* 首先给出本程序语言的具体文法
+
+```
+ Program -> MainClass ClassDec*
+
+ MainClass -> class Id { void main() { Statement }}
+
+ ClassDec -> class Id { VarDec* MethodDec* }
+          -> class Id : Id { VarDec* MethodDec* }
+         
+ VarDec -> Type Id;
+ 
+ MethodDec -> Type Id (FormalList) 
+              { VarDec* Statement* return Exp;}
+
+ FromalList -> Type Id FormalRest*
+            -> 
+ 
+ FormalRest -> , Type Id
+ 
+ Type -> int
+      -> boolean
+      -> Id
+ 
+ 
+ Statement -> { Statement* }
+           -> if (Exp) Statement else Statement
+           -> while (Exp) Statement
+           -> print(Exp);
+           -> Id = Exp;
+           -> Id[Exp] = Exp;
+ 
+ Exp -> Exp op Exp
+     -> Exp.Id(ExpList)
+     -> Integer Literal
+     -> true
+     -> false
+     -> Id
+     -> this
+     -> new Id()
+     -> !Exp
+     -> (Exp)
+     
+ Op -> +
+    -> *
+    -> < 
+    -> &&
+    
+ ExpList -> Exp ExpRest*
+         -> 
+  
+ ExpRest -> ,Exp
+ 
+ Id -> [A-Za-z_][A-Za-z0-9_]*
+ 
+ Integer Literal -> [0-9]+
+ 
+ LineComment -> // the total line is comment
+ 
+```   
+   
+* 由以上文法给出的程序样例
+
+```
+class TestMain 
+{
+    void main()
+    {
+        print(new Test().Compute(10));   
+    }
+}
+
+class Test
+{
+    int Compute(int num)
+    {
+        int total;
+        if ( num < 1)
+            total = 1;
+        else 
+            total = num * (this.Compute(num-1));
+        return total;
+    }
+}
+```
---
+++ src/lexer/Token.java
@@ -0,0 +1,67 @@
+package lexer;
+
+/**
+ * Created by Mengxu on 2017/1/6.
+ */
+public class Token
+{
+    public enum Kind
+    {
+        Add, // +
+        And, // &&
+        Assign, // =
+        Boolean, // boolean
+        Class, // class
+        Colon, // :
+        Commer, // ,
+        Dot, // .
+        Else, // else
+        EOF, // End of file
+        False, // false
+        ID, // Identifier
+        If, // if
+        Int, // int
+        Lbrace, // {
+        Lparen, // (
+        LT, // <
+        Main, // main
+        New, // new
+        Not, // !
+        NUM, // Integer literal
+        Print, // print
+        Rbrace, // }
+        Return, // return
+        Rparen, // )
+        Semi, // ;
+        This, // this
+        Times, // *
+        True, // true
+        Void, // void
+        While // while
+    }
+
+    public Kind kind; // the kind of the token
+    public String lexeme; // extra lexeme of the token
+    public int lineNum; // the line number of the token
+
+    public Token(Kind kind, int lineNum)
+    {
+        this.kind = kind;
+        this.lineNum = lineNum;
+    }
+
+    public Token(Kind kind, int lineNum, String lexeme)
+    {
+        this.kind = kind;
+        this.lineNum = lineNum;
+        this.lexeme = lexeme;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.kind.toString()
+                + " : " + (lexeme == null ? "" : this.lexeme)
+                + " : at line " + this.lineNum;
+    }
+}
