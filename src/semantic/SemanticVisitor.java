e2b2fcdd41b4b247653e41c5f23e6e550ccfa223
Mengxu <taomengxu@126.com>
2017-01-13 19:14:00 +0800

    Init semantic analysis Complete ClassBinding ClassTable MethodType MethodTable

---
+++ src/semantic/SemanticVisitor.java
@@ -0,0 +1,165 @@
+package semantic;
+
+import ast.Ast;
+
+/**
+ * Created by Mengxu on 2017/1/13.
+ */
+public class SemanticVisitor implements ast.Visitor
+{
+    @Override
+    public void visit(Ast.Type.Boolean t)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Type.ClassType t)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Type.Int t)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Dec.DecSingle d)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Add e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.And e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Call e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.False e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Id e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.LT e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.NewObject e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Not e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Num e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Sub e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.This e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.Times e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Exp.True e)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Stm.Assign s)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Stm.Block s)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Stm.If s)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Stm.Print s)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Stm.While s)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Method.MethodSingle m)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Class.ClassSingle c)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.MainClass.MainClassSingle c)
+    {
+
+    }
+
+    @Override
+    public void visit(Ast.Program.ProgramSingle p)
+    {
+
+    }
+}
