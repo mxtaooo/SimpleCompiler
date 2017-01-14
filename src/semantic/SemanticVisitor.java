806197c659e106f9188896dde5699c79f5b04a25
Mengxu <taomengxu@126.com>
2017-01-14 10:41:41 +0800

    Complete semantic visitor

--- src/semantic/SemanticVisitor.java
+++ src/semantic/SemanticVisitor.java
@@ -2,164 +2,403 @@ package semantic;
 
 import ast.Ast;
 
+import java.util.LinkedList;
+
+
 /**
  * Created by Mengxu on 2017/1/13.
  */
 public class SemanticVisitor implements ast.Visitor
 {
-    @Override
-    public void visit(Ast.Type.Boolean t)
-    {
+    private ClassTable classTable;
+    private MethodVariableTable methodVarTable;
+    private String currentClass;
+    private Ast.Type.T type;
 
+    public SemanticVisitor()
+    {
+        this.classTable = new ClassTable();
+        this.methodVarTable = new MethodVariableTable();
+        this.currentClass = null;
+        this.type = null;
     }
 
-    @Override
-    public void visit(Ast.Type.ClassType t)
+    private void error()
     {
-
+        System.out.print("Error: ");
     }
 
-    @Override
-    public void visit(Ast.Type.Int t)
+    private boolean isMatch(Ast.Type.T target, Ast.Type.T cur)
     {
-
+        if (target.toString().equals(cur.toString()))
+            return true;
+        else if (target instanceof Ast.Type.ClassType && cur instanceof Ast.Type.ClassType)
+        {
+            String tarName = ((Ast.Type.ClassType) target).id;
+            String curName = ((Ast.Type.ClassType) cur).id;
+            boolean flag = tarName.equals(curName);
+            while (curName != null && !flag)
+            {
+                curName = classTable.getClassBinding(curName).base;
+                flag = tarName.equals(curName);
+            }
+            return flag;
+        } else return false;
     }
 
+    // Type
     @Override
-    public void visit(Ast.Dec.DecSingle d)
-    {
+    public void visit(Ast.Type.Boolean t) {}
 
-    }
+    @Override
+    public void visit(Ast.Type.ClassType t) {}
 
+    @Override
+    public void visit(Ast.Type.Int t) {}
+
+    // Dec
+    @Override
+    public void visit(Ast.Dec.DecSingle d) {}
+
+    // Exp
     @Override
     public void visit(Ast.Exp.Add e)
     {
-
+        this.visit(e.left);
+        Ast.Type.T lefty = this.type;
+        this.visit(e.right);
+        if (!this.type.toString().equals(lefty.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " the type of left is" + lefty.toString() +
+                    ", but the type of right is" + this.type.toString());
+        } else if (!new Ast.Type.Int().toString().equals(this.type.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " only integer numbers can be added.");
+        }
+        this.type = new Ast.Type.Int();
     }
 
     @Override
     public void visit(Ast.Exp.And e)
     {
-
+        this.visit(e.left);
+        Ast.Type.T lefty = this.type;
+        this.visit(e.right);
+        if (!this.type.toString().equals(lefty.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " the type of left is" + lefty.toString() +
+                    ", but the type of right is" + this.type.toString());
+        } else if (!new Ast.Type.Boolean().toString().equals(this.type.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " only integer numbers can be added.");
+        }
+        this.type = new Ast.Type.Boolean();
     }
 
     @Override
     public void visit(Ast.Exp.Call e)
     {
+        this.visit(e.exp);
+        Ast.Type.ClassType expType = null;
 
+        if (this.type instanceof Ast.Type.ClassType)
+        {
+            expType = ((Ast.Type.ClassType) this.type);
+            e.type = expType.id;
+        } else
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    "only an instance of class can be invoked.");
+            this.type = new Ast.Type.T() {};
+            return;
+        }
+
+        LinkedList<Ast.Type.T> argsty = new LinkedList<>();
+        for (Ast.Exp.T arg : e.args)
+        {
+            this.visit(arg);
+            argsty.addLast(this.type);
+        }
+
+        MethodType mty = this.classTable.getMethodType(expType.id, e.id);
+        if (mty.argsType.size() != argsty.size())
+        {
+            error();
+            System.out.println("Line " + e.lineNum + " the count of arguments is not match ");
+        }
+        for (int i = 0; i < mty.argsType.size(); i++)
+        {
+            if (!isMatch(((Ast.Dec.DecSingle) mty.argsType.get(i)).type, argsty.get(i)))
+            {
+                error();
+                System.out.println("Line " + e.args.get(i).lineNum + " the parameter " + i +
+                        "needs a " + ((Ast.Dec.DecSingle) mty.argsType.get(i)).type.toString() +
+                        ", but got a" + argsty.get(i).toString());
+            }
+        }
+        e.at = argsty;
+        this.type = mty.retType;
     }
 
     @Override
     public void visit(Ast.Exp.False e)
     {
-
+        this.type = new Ast.Type.Boolean();
     }
 
     @Override
     public void visit(Ast.Exp.Id e)
     {
+        Ast.Type.T type = this.methodVarTable.get(e.id);
+        String className = currentClass;
+        while (type == null && className != null)
+        {
+            type = this.classTable.getFieldType(className, e.id);
+            className = this.classTable.getClassBinding(className).base;
+        }
 
+        if (type == null)
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    "you should declare is before use it.");
+            e.type = new Ast.Type.T() {};
+            this.type = e.type;
+        } else
+        {
+            e.isField = true;
+            e.type = type;
+            this.type = type;
+        }
     }
 
     @Override
     public void visit(Ast.Exp.LT e)
     {
-
+        this.visit(e.left);
+        Ast.Type.T lefty = this.type;
+        this.visit(e.right);
+        if (!this.type.toString().equals(lefty.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " the type of left is" + lefty.toString() +
+                    ", but the type of right is" + this.type.toString());
+        } else if (!new Ast.Type.Int().toString().equals(this.type.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " only integer numbers can be compared.");
+        }
+        this.type = new Ast.Type.Boolean();
     }
 
     @Override
     public void visit(Ast.Exp.NewObject e)
     {
-
+        this.type = new Ast.Type.ClassType(e.id);
     }
 
     @Override
     public void visit(Ast.Exp.Not e)
     {
-
+        this.visit(e.exp);
+        if (!this.type.toString().equals(new Ast.Type.Boolean().toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " the exp cannot calculate to a boolean.");
+        }
+        this.type = new Ast.Type.Boolean();
     }
 
     @Override
     public void visit(Ast.Exp.Num e)
     {
-
+        this.type = new Ast.Type.Int();
     }
 
     @Override
     public void visit(Ast.Exp.Sub e)
     {
-
+        this.visit(e.left);
+        Ast.Type.T lefty = this.type;
+        this.visit(e.right);
+        if (!this.type.toString().equals(lefty.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " the type of left is" + lefty.toString() +
+                    ", but the type of right is" + this.type.toString());
+        } else if (!new Ast.Type.Int().toString().equals(this.type.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " only integer numbers can be subbed.");
+        }
+        this.type = new Ast.Type.Int();
     }
 
     @Override
     public void visit(Ast.Exp.This e)
     {
-
+        this.type = new Ast.Type.ClassType(currentClass);
     }
 
     @Override
     public void visit(Ast.Exp.Times e)
     {
-
+        this.visit(e.left);
+        Ast.Type.T lefty = this.type;
+        this.visit(e.right);
+        if (!this.type.toString().equals(lefty.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " the type of left is" + lefty.toString() +
+                    ", but the type of right is" + this.type.toString());
+        } else if (!new Ast.Type.Int().toString().equals(this.type.toString()))
+        {
+            error();
+            System.out.println("Line " + e.lineNum +
+                    " only integer numbers can be timed.");
+        }
+        this.type = new Ast.Type.Int();
     }
 
     @Override
     public void visit(Ast.Exp.True e)
     {
-
+        this.type = new Ast.Type.Boolean();
     }
 
     @Override
     public void visit(Ast.Stm.Assign s)
     {
-
+        Ast.Exp.Id id = new Ast.Exp.Id(s.id, s.lineNum);
+        this.visit(id);
+        Ast.Type.T idty = this.type;
+        this.visit(s.exp);
+        s.type = this.type;
+        if (!this.type.toString().equals(idty.toString()))
+        {
+            error();
+            System.out.println("Line " + s.lineNum +
+                    " the type of id is" + idty.toString() +
+                    ", but the type of expression is" + this.type.toString() +
+                    ". Assign failed!");
+        }
     }
 
     @Override
     public void visit(Ast.Stm.Block s)
     {
-
+        s.stms.forEach(this::visit);
     }
 
     @Override
     public void visit(Ast.Stm.If s)
     {
-
+        this.visit(s.condition);
+        if (!this.type.toString().equals(new Ast.Type.Boolean().toString()))
+        {
+            error();
+            System.out.println("Line " + s.condition.lineNum +
+                    " the condition's type should be a boolean.");
+        }
+        this.visit(s.then_stm);
+        this.visit(s.else_stm);
     }
 
     @Override
     public void visit(Ast.Stm.Print s)
     {
-
+        this.visit(s.exp);
+        if (!this.type.toString().equals(new Ast.Type.Int().toString()))
+        {
+            error();
+            System.out.println("Line " + s.exp.lineNum +
+                    " the exp must be a integer or can be calculate to an integer");
+        }
     }
 
     @Override
     public void visit(Ast.Stm.While s)
     {
-
+        this.visit(s.condition);
+        if (!this.type.toString().equals(new Ast.Type.Boolean().toString()))
+        {
+            error();
+            System.out.println("Line " + s.condition.lineNum +
+                    " the condition's type should be a boolean.");
+        }
+        this.visit(s.body);
     }
 
     @Override
     public void visit(Ast.Method.MethodSingle m)
     {
-
+        this.methodVarTable = new MethodVariableTable();
+        this.methodVarTable.put(m.formals, m.decs);
+        m.stms.forEach(this::visit);
+        this.visit(m.retExp);
+        if (!this.type.toString().equals(m.retType.toString()))
+        {
+            error();
+            System.out.println("Line " + m.retExp.lineNum +
+                    "the return expression's type is not match the method " +
+                    m.id + " declared.");
+        }
     }
 
     @Override
     public void visit(Ast.Class.ClassSingle c)
     {
-
+        this.currentClass = c.id;
+        c.methods.forEach(this::visit);
     }
 
     @Override
     public void visit(Ast.MainClass.MainClassSingle c)
     {
-
+        this.currentClass = c.id;
+        this.visit(c.stm);
     }
 
     @Override
     public void visit(Ast.Program.ProgramSingle p)
     {
+        // put main class to class table
+        this.classTable.putClassBinding(((Ast.MainClass.MainClassSingle) p.mainClass).id,
+                new ClassBinding(null));
 
+        for (Ast.Class.T c : p.classes)
+        {
+            Ast.Class.ClassSingle cla = ((Ast.Class.ClassSingle) c);
+            this.classTable.putClassBinding(cla.id, new ClassBinding(cla.base));
+            for (Ast.Dec.T field : cla.fields)
+            {
+                this.classTable.putFieldToClass(cla.id, ((Ast.Dec.DecSingle) field).id,
+                        ((Ast.Dec.DecSingle) field).type);
+            }
+            for (Ast.Method.T method : cla.methods)
+            {
+                Ast.Method.MethodSingle m = ((Ast.Method.MethodSingle) method);
+                this.classTable.putMethodToClass(cla.id, m.id,
+                        new MethodType(m.retType, m.decs));
+            }
+        }
+
+        this.visit(p.mainClass);
+        p.classes.forEach(this::visit);
     }
 }
