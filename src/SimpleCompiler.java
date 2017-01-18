a3b760fd80a3255160023d1c265bcca96301e5bf
Mengxu <taomengxu@126.com>
2017-01-18 17:45:29 +0800

    make main class usable and add META-INF for export to a jar file

--- src/SimpleCompiler.java
+++ src/SimpleCompiler.java
@@ -1,3 +1,14 @@
+import ast.Ast;
+import codegen.ByteCodeGenerator;
+import codegen.TranslatorVisitor;
+import parser.Parser;
+import semantic.SemanticVisitor;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 /**
  * Created by Mengxu on 2017/1/4.
  */
@@ -9,8 +20,36 @@ public class SimpleCompiler
         {
             System.out.println("Hello, this is a simple compiler!");
             System.out.println("Please input the file name which you want to compile");
+            System.exit(0);
         }
-        else
-            System.out.println(args[0]);
+
+        final String fname = args[0];
+        InputStream fstream = null;
+        try
+        {
+            fstream = new BufferedInputStream(new FileInputStream(fname));
+        } catch (FileNotFoundException e)
+        {
+            System.out.println("Cannot find the file: " + fname);
+            System.exit(1);
+        }
+
+        Parser parser = new Parser(fstream);
+        Ast.Program.T prog = parser.parse();
+
+        SemanticVisitor checker = new SemanticVisitor();
+        checker.visit(prog);
+
+        TranslatorVisitor translator = new TranslatorVisitor();
+        translator.visit(prog);
+
+        ByteCodeGenerator generator = new ByteCodeGenerator();
+        generator.visit(translator.prog);
+
+        // ascii instructions to binary file
+        jasmin.Main.main(new String[]{translator.prog.mainClass.id + ".il"});
+        for (codegen.ast.Ast.Class.ClassSingle cla : translator.prog.classes)
+            jasmin.Main.main(new String[]{cla.id + ".il"});
+
     }
 }
